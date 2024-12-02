34ef8d3a2eeb23f95a32212f7037e5cd00763a0b
Brian-Bowyer <bobsnake64@gmail.com>
2024-12-01 20:40:27 -0600

    first commit

---
+++ day_1/main.py
@@ -0,0 +1,26 @@
+def process_file(filename: str):
+    with open(filename, "r") as f:
+        lines = f.readlines()
+        list1, list2 = [], []
+        for i in range(len(lines)):
+            lines[i] = lines[i].strip()
+            split_line = lines[i].split(" ")
+            list1.append(int(split_line[0]))
+            list2.append(int(split_line[-1]))
+
+        return list1, list2
+
+
+def main1():
+    l1, l2 = process_file("input.txt")
+    l1_sorted = sorted(l1)
+    l2_sorted = sorted(l2)
+    distances = []
+    for pair in zip(l1_sorted, l2_sorted):
+        distance = abs(pair[0] - pair[1])
+        distances.append(distance)
+    print(sum(distances))
+
+
+if __name__ == "__main__":
+    main1()

3d8f0eb7604e7a7a8444b87a6f92a21446ede5a7
Brian-Bowyer <bobsnake64@gmail.com>
2024-12-02 22:21:31 -0600

    day 2 part 2 correct answer

--- day_2/main.py
+++ day_2/main.py
@@ -8,30 +8,30 @@ def compute_diffs(report: list[int]):
     return [num - report[last_index] for last_index, num in enumerate(report[1:])]
 
 
-def check_diffs(diff: list[int]):
-    if diff[0] <= 0 or diff[1] <= 0:
+def check_diffs(diff: list[int], dampener: int = 0):
+    if diff[0] < 0:
         diff = [-1 * val for val in diff]
 
-    dampened = False
-    for i, val in enumerate(diff):
-        if 0 < val < 4:
-            continue
-        elif dampened == False:
-            dampened = True
-            if 0 < val + diff[i - 1] < 4:
-                continue
-            elif i + 1 < len(diff) and 0 < val + diff[i + 1] < 4:
-                continue
-            else:
-                return False
-        else:
-            return False
+    return all(0 < val < 4 for val in diff)
 
-    return True
+
+def get_best(report):
+    if check_diffs(compute_diffs(report)):
+        return report
+    else:
+        for i in range(len(report)):
+            new_report = report.copy()
+            new_report.pop(i)
+            if check_diffs(compute_diffs(new_report)):
+                return new_report
+        return report  # it's gonna fail but we need to return something
 
 
 if __name__ == "__main__":
-    reports = process_file("input.txt")
+    reports = process_file("basic_input.txt")
     diffs = [compute_diffs(report) for report in reports]
     valids = [check_diffs(diff) for diff in diffs]
     print(valids.count(True))
+
+    best_valids = [check_diffs(compute_diffs(get_best(report))) for report in reports]
+    print(best_valids.count(True))
